4ca6f2834a78de5417883526b6c8c59ec96ba377
bhawnarathor1204 <brathor@sebonetechnologies.com>
2022-10-08 19:43:30 +0530

    hey

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello this is my page</h1>
+     <h1>Hello this is my page</h1>
+     <h1>Hello this is my page</h1>
+    </div>
+  );
+}
+
+export default App;
